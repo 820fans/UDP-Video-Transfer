826a5f6c93cc176187d8347c0720a70dae271a26
Yuxiang Chen <chenyx.cs@gmail.com>
2019-01-22 16:45:45 +0800

    python & tcp & opencv
    demo1

--- _yuxiang/client.py
+++ _yuxiang/client.py
@@ -1,28 +1,33 @@
 import socket 
 import cv2 
-import numpy 
-
-sk = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-
-host = socket.gethostname() # 获取本地主机名
-port = 12345                # 设置端口号
+import simplejson
+import numpy as np
 
 
-# 主动初始化TCP服务器连接，。一般address的格式为元组（hostname,port），
-# 如果连接出错，返回socket.error错误。
-sk.connect((host, port))
-
-capture = cv2.VideoCapture(0)
-encode_param=[int(cv2.IMWRITE_JPEG_QUALITY),90] #设置编码参数
+def send_one_frame(sk):
+    ret, frame = cap.read()
+    sk.send(frame.tobytes())
 
 
-# while True
-ret, frame = capture.read()
-result, imgencode = cv2.imencode('.jpg', frame)
-data = numpy.array(imgencode)
-stringData = data.tostring()
-print(type(stringData))
-for i in range(0,len(stringData)):
-    sk.send(stringData[i])
+if __name__ == '__main__':
+    sk = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
+    host = socket.gethostname() # 获取本地主机名
+    port = 1230               # 设置端口号
+
+    # 主动初始化TCP服务器连接，。一般ad dress的格式为元组（hostname,port），
+    # 如果连接出错，返回socket.error错误。
+    sk.connect((host, port))
     
+    cap = cv2.VideoCapture(0)
+
+    count = 10000
+
+    while count:
+        send_one_frame(sk)
+        count = count - 1
+        if count % 200 == 0 :
+            print("{}帧".format(10000-count))
+
+    sk.close()
+    cap.release()
--- _yuxiang/server.py
+++ _yuxiang/server.py
@@ -1,28 +1,51 @@
 import socket 
 import cv2 
+import numpy as np 
+# import simplejson
 
-sk = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-# socket.socket([family[, type[, proto]]])
-# family: 套接字家族可以使AF_UNIX或者AF_INET
-# type: 套接字类型可以根据是面向连接的还是非连接分为SOCK_STREAM或SOCK_DGRAM
-# protocol: 一般不填默认为0.
 
-host = socket.gethostname() # 获取本地主机名
-port = 12345                # 设置端口
+def recv_one_frame(conn):
+    k = conn.recv(buffersize)
+    try:
+        k = np.frombuffer(k, dtype=np.uint8).reshape(h, w, c)
+        return k
+    except:
+        return False
 
-sk.bind((host,port))
-sk.listen(5)
-# 开始TCP监听。backlog指定在拒绝连接之前，
-# 操作系统可以挂起的最大连接数量。该值至少为1，大部分应用程序设为5就可以了。
 
-buffersize = 1024
-print("waiting for the client...")
 
-while True:
+if __name__ == '__main__':
+    h = 480
+    w = 640
+    c = 3
+    buffersize = 3*640*480
+    sk = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    # socket.socket([family[, type[, proto]]])
+    # family: 套接字家族可以使AF_UNIX或者AF_INET
+    # type: 套接字类型可以根据是面向连接的还是非连接分为SOCK_STREAM或SOCK_DGRAM
+    # protocol: 一般不填默认为0.
+
+    host = socket.gethostname() # 获取本地主机名
+    port = 1230                # 设置端口
+
+    sk.bind((host,port))
+    sk.listen(2)
+    # 开始TCP监听。backlog指定在拒绝连接之前，
+    # 操作系统可以挂起的最大连接数量。该值至少为1，大部分应用程序设为5就可以了。
+
+    print("waiting for the client...")
     conn, address = sk.accept()# 被动接受TCP客户端连接,(阻塞式)等待连接的到来
-    # address是地址
-    frame = conn.recv(buffersize)
-    print(frame)
-    conn.close()
 
-sk.close()
+    while True:
+        frame = recv_one_frame(conn)
+        if frame is False :
+            continue
+            print("网络不稳定！")
+        else :
+            cv2.imshow('test', frame)
+            if (cv2.waitKey(1) & 0xFF) == ord('q'):
+                break
+
+    conn.close()
+    sk.close()
+    cv2.destroyAllWindows()
